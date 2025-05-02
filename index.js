c8d6537bed0e360844866be3ae3e5c7ef1f01d8b
modguy07 <206636428+oneshotenjoyer@users.noreply.github.com>
2025-05-01 20:29:59 -0400

    Add site files

---
+++ index.js
@@ -0,0 +1,7 @@
+var modrinth = function () {
+    window.open("https://modrinth.com/user/modguy07", "_blank");
+}
+
+var repos = function () {
+    window.open("https://github.com/oneshotenjoyer", "_blank");
+}
